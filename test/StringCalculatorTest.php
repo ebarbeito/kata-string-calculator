f7fae99abd9c109c0c032a2dcbbb4a30d5e243fc
Enrique Barbeito García <ebarbeito@gmail.com>
2017-01-17 23:49:42 +0100

    Refactor extract to method formatNumbers within test suite

--- test/StringCalculatorTest.php
+++ test/StringCalculatorTest.php
@@ -21,17 +21,23 @@ class StringCalculatorTest extends \PHPUnit_Framework_TestCase
 
     public function test_single_number_should_return_same_number()
     {
-        $this->assertEquals(8, $this->calculator->Add('8'));
+        $this->assertEquals(8, $this->calculator->Add(
+          $this->formatNumbers(8)
+        ));
     }
 
     public function test_two_numbers_should_return_their_sum()
     {
-        $this->assertEquals(288, $this->calculator->Add('32,256'));
+        $this->assertEquals(288, $this->calculator->Add(
+          $this->formatNumbers(32, 256)
+        ));
     }
 
     public function test_multiple_numbers_should_return_their_sum()
     {
-        $this->assertEquals(30, $this->calculator->Add('0,2,4,8,16'));
+        $this->assertEquals(30, $this->calculator->Add(
+          $this->formatNumbers(0, 2, 4, 8, 16)
+        ));
     }
 
     public function test_Add_method_should_have_commutative_property()
@@ -40,8 +46,18 @@ class StringCalculatorTest extends \PHPUnit_Framework_TestCase
         $four = 4;
 
         $this->assertEquals(
-          $this->calculator->Add(sprintf('%d%s%d', $eight, StringCalculator::DELIMITER, $four)),
-          $this->calculator->Add(sprintf('%d%s%d', $four, StringCalculator::DELIMITER, $eight))
+          $this->calculator->Add($this->formatNumbers($eight, $four)),
+          $this->calculator->Add($this->formatNumbers($eight, $four))
         );
+    }
+
+    /**
+     * @param array ...$numbers
+     *
+     * @return string
+     */
+    private function formatNumbers(...$numbers): string
+    {
+        return implode(StringCalculator::DELIMITER, $numbers);
     }
 }
